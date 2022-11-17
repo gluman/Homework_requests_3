1766db54b01cadf4bca10e2f914012ea93b37e75
Andrey Glumov <gluman@yandex.ru>
2022-11-17 16:56:44 +0300

    task complete may be

--- main.py
+++ main.py
@@ -22,25 +22,29 @@ class Stackoverflow:
 
     def get_questions_limit(self, diff_days):
         uri = '2.3/questions'
-        today = d.datetime.now().timestamp()  # now date
+        today = d.datetime.now().timestamp()  # now date in mil sec
         delta_days = d.timedelta(days=diff_days).total_seconds()  # diff 2 days in mil sec
-        date_eirler = str(int(round(today - delta_days, 0)))
+        date_eirler = str(int(round(today - delta_days, 0)))  # date 2 days orlse
 
-        print(date_eirler)
         url = self.base_host + uri
         params = {'order': 'asc',
-                  'min': date_eirler,
+                  'fromdate': date_eirler,
                   'sort': 'creation',
                   'site': 'stackoverflow',
                   'filter': '!Oev7Wy_R1VbJ_Uiy-DLg_H7(uGMFWHCMUHTbs(7dTS6'
                   }
         res = rq.get(url, params=params)
+
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_rows', None)
-        pp(pd.DataFrame(res.json()['items']))
+        res_df = pd.DataFrame(res.json()['items'])
+        pp(len(res_df['title']))
+        res_df['creation_date'] = pd.to_datetime(res_df['creation_date'], unit='s')
+
+        pp(res_df)
         # pp(res.json())
 
 
 if __name__ == '__main__':
     questions = Stackoverflow()
-    questions.get_questions_limit(2)
+    questions.get_questions_limit(3)
